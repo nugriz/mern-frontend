cb7fa66929ee68233c79460571b68e75c146ef60
nugriz <rizki@happy5.co>
2024-02-26 14:04:03 +0700

    firxt commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('Buat')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sample Blog</h1>
+        <div onClick={setData('Buat Lagi')} id='add'><h6 style={{margin:0}}>{{data}}</h6></div>
+        <div id='blog'>
+        </div>
       </header>
     </div>
   );
